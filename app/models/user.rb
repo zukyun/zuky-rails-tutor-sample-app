0d154630eb030ebfcee0ea04a624111af08e400b
Dan Moran <danieljmoranjr@gmail.com>
2012-09-23 17:31:39 -0400

    chapter 8 interim commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name, 
     presence: true, 
@@ -34,5 +35,11 @@ class User < ActiveRecord::Base
     
   validates :password_confirmation, 
     presence: true 
+    
+  private
+  
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 
 end
