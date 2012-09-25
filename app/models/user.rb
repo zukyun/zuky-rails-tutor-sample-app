e9e5225f976e95f1cf990464ac801d747cadd464
Dan Moran <danieljmoranjr@gmail.com>
2012-09-25 16:43:30 -0400

    chapter 11 interim commit 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,11 @@ class User < ActiveRecord::Base
   
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", 
+    class_name: "Relationship", dependent: :destroy 
+  has_many :followed_users, through: :relationships, source: :followed 
+  has_many :followers, through: :reverse_relationships, source: :follower
   
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -40,6 +45,18 @@ class User < ActiveRecord::Base
   def feed 
     # preliminary
     Micropost.where("user_id = ?", id)
+  end
+  
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+  
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+  
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
     
   private
